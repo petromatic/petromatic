6d3e230ad8e675b5d8dbc8f9e168320dc09ffc7c
Nicolás Dazeo <nicodaze@gmail.com>
2017-11-07 20:15:11 -0300

    Serial test

--- test.py
+++ test.py
@@ -3,6 +3,18 @@
 from devices.httpDevice import httpDevice
 from states.stateMachine import StateMachine
 
+#h = httpDevice(8080)
+#s = StateMachine()
+#h.suscribe(lambda e,a: s.do(e,a))
+
+from serial import Serial
+
+ser = Serial('/dev/ttyACM0')
+from devices.flowMeter import FlowMeter
+
+f = FlowMeter(ser)
+f.suscribe(lambda e,a: print(e,a))
+
 h = httpDevice(8080)
 s = StateMachine()
 h.suscribe(lambda e,a: s.do(e,a))
