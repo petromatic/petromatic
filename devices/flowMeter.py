545f26b2b48ccb412fd6eb8764b3bc905bc5f0ca
Nicolás Dazeo <nicodaze@gmail.com>
2017-11-07 20:14:37 -0300

    Completed flow meter read

--- devices/flowMeter.py
+++ devices/flowMeter.py
@@ -1,20 +1,31 @@
+"""
+    FlowMeter
+"""
 from threading import Thread
 from time import sleep
 
 class FlowMeter(Thread):
+    """
+        FlowMeter class
+    """
     def __init__(self, serial):
         super(FlowMeter, self).__init__()
         self.serial = serial
         self.daemon = True
+        self.eventListeners = []
         self.start()
+
+    def suscribe(self, listener):
+        """Suscribe to events"""
+        self.eventListeners += [listener]
 
     def run(self):
         v0 = 0.0
         while True:
-            self.serial.write(b'')
-            v = float(ser.readline()) # Needs timeout in serial for security
+            self.serial.write(b'FM+ACM\r\n')
+            v = float(self.serial.readline()) # Needs timeout in serial for security
             if v != v0:
-                raiseEvent("FlowMeterChange",[v])
+                self.raiseEvent("FlowMeterChange",[v])
             v0 = v
             sleep(0.125)
     
