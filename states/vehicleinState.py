f01047751ef46ce2d0b64747fe81534096dc87ce
Nicolás Dazeo <nicodaze@gmail.com>
2017-11-25 15:06:20 -0300

    Added RFID EM access

--- states/vehicleinState.py
+++ states/vehicleinState.py
@@ -1,8 +1,16 @@
 from .state import State
+from .station import Station
 
 class VehicleinState(State):
     def __init__(self):
         super(VehicleinState, self).__init__()
+        self.station = Station.get()
+        self.station.rfid_em.on()
+
+    def onRfidEMRead(self, rfid):
+        from .fillState import FillState
+        self.station.rfid_em.ack()
+        return FillState()
 
     def onPumpAccessRequest(self):
         from .fillState import FillState
@@ -10,5 +18,6 @@ class VehicleinState(State):
 
     def onExitRequest(self):
         print(__file__ + " TODO: Open exit gate")
+        self.station.rfid_em.off()
         from .exitState import ExitState
         return ExitState()
