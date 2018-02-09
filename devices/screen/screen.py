6362ad109f1bd05982fcf84681e2f23d45f4da93
Nicolás Dazeo <ndazeo@pladema.net>
2018-02-09 15:23:57 -0300

    Screen is now functional

--- devices/screen/screen.py
+++ devices/screen/screen.py
@@ -2,53 +2,69 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from ..observable import Observable
 
 class Driver(object):
     def __init__(self):
-        super(Driver, self).__init__()
-        self.name = "Nicolás Dazeo"
-        self.dni = "36383780"
+        super().__init__()
+        self.name = ""
+        self.dni = ""
+    
+    def setName(name):
+        self.name = name
+    
+    def setDNI(dni):
+        self.dni = dni
 
 class Vehicle(object):
     def __init__(self):
-        super(Vehicle, self).__init__()
-        self.name = "Ford Cargo 916"
-        self.plate = "EFN 573"
+        super().__init__()
+        self.name = ""
+        self.plate = ""
+    
+    def setName(name):
+        self.name = name
+    
+    def setPlate(plate):
+        self.plate = plate
 
-class ControlWindow(GridLayout):
+class FillWindow(GridLayout, Observable):
     def __init__(self, *args, **kwargs):
-        super(ControlWindow, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.eventListeners = []
         self.driver = Driver()
         self.vehicle = Vehicle()
-        
+        self.liters = 0
+
+    def setVehicle(self, vehicle):
+        self.vehicle = vehicle
+
+    def setDriver(self, driver):
+        self.driver = driver
+
+    def setLiters(self, liters):
+        self.liters = liters
+
     def clicked(self):
-        self.raiseEvent("ExitButton",[])
+        self.raiseEvent("ExitButtonClick",[])
 
-    def raiseEvent(self, event, args):
-        for listener in self.eventListeners:
-            listener(event, args)
-
-    def suscribe(self, listener):
-        """Suscribe to events"""
-        self.eventListeners += [listener]
-
-class Screen(object):
-    def __init__(self):
-        super(Screen, self).__init__()
-        KvApp().run()
-
-class KvApp(App):
+class Screen(App, Observable):
     def __init__(self, **kwargs):
-        super(KvApp, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+            
+    def setDriver(self, driver):
+        self.win.setDriver(driver)
+
+    def setVehicle(self, vehicle):
+        self.win.setVehicle(vehicle)
+    
+    def setLiters(self, liters):
+        self.win.setLiters(self, liters)
 
     def build(self):
-        win = Builder.load_file("screen.kv")
-        win.suscribe(self.event)
-        return win
-    
-    def event(self, name, args):
-        print("event: "+name)
+        self.win = Builder.load_file("FillWindow.kv")
+        self.win.suscribe(self.raiseEvent)
+        return self.win
 
 if __name__ == '__main__':
-    KvApp().run()
+    Screen().run()
