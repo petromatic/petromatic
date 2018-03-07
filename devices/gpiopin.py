ff265ad0d10b9a8ca1c8db8955bcf08b6b916582
Nicolás Dazeo <ndazeo@pladema.net>
2018-03-07 14:38:03 -0300

    Init and exit on library

--- devices/gpiopin.py
+++ devices/gpiopin.py
@@ -1,17 +1,18 @@
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+
 class GPIOPin(Object):
     def __init__(self, pin):
         super().__init__()
         self._pin = pin
         GPIO.setup(self._pin, GPIO.OUT)
-
-    def __enter__(self):
-        GPIO.setmode(GPIO.BCM)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        GPIO.cleanup()
-
+        
     def setValue(self, value):
         GPIO.output(self._pin, value)
+
+def cleanup():
+    GPIO.cleanup()
+
+import atexit
+atexit.register(cleanup)
