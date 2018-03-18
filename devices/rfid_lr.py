113db7b0eb91f9c1b684c0e23736f7912ee52f9f
Nicolás Dazeo <nicodazeo@gmail.com>
2018-03-18 13:00:39 -0300

    Added RFIDLR EPC read tag documentation

--- devices/rfid_lr.py
+++ devices/rfid_lr.py
@@ -23,6 +23,11 @@ class RfidLR(Thread, Observable):
         self.start()
 
     def run(self):
+        # https://drive.google.com/file/u/0/d/1peIpY11eZ45KJWxPZpztpvczqKaRXnvH/view?usp=drive_web
+        # 4.4. EPC(GEN 2) Identify Single Tag
+        # HEAD ADDR(LSB) ADDR(MSB) CID1 RTN LENGTH AN D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D0 D1 CHKSUM
+        # CC   FF        FF        10   00  0D     01 E2 00 34 11 B8 02 01 13 83 25 85 66 0xNN
+
         msg = b'\x7C\xFF\xFF\x10\x32\x00'
 
         while True:
