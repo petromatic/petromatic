efbdece62312bbdc3516b02ac480ac1ee5ef6edd
Nicolas Dazeo <ndazeo@gmail.com>
2018-03-27 17:39:44 +0000

    Set firebase url

--- accounts/ffAccountManager.py
+++ accounts/ffAccountManager.py
@@ -6,7 +6,8 @@ import json
 class FfAccountManager(AccountManager):
     def __init__(self):
         super(FfAccountManager, self).__init__()
-        self._url = 'petromatic-4240b.firebaseio.com'
+        # self._url = 'petromatic-4240b.firebaseio.com'
+        self._url = 'us-central1-petromatic-4240b.cloudfunctions.net'
         # self._url = 'localhost'
         self._tid = '797647ef89c8ddb64d382bc4c87e980de7328fb8'
 
@@ -14,10 +15,10 @@ class FfAccountManager(AccountManager):
         return True
 
     def getUserByRFID(self, rfidlr, rfidem):
-        conn = http.client.HTTPSConnection(self._url, 5000)
+        conn = http.client.HTTPSConnection(self._url)
         #conn = http.client.HTTPConnection(self._url, 5000)
-        print("/petromatic-4240b/us-central1/validateDriver/"+"/".join([rfidlr, rfidem]))
-        conn.request("GET", "/petromatic-4240b/us-central1/validateDriver/"+"/".join([rfidlr, rfidem]))
+        print(self._url +"/validateDriver/"+"/".join([rfidlr, rfidem]))
+        conn.request("GET", "/validateDriver/"+"/".join([rfidlr, rfidem]))
         res = conn.getresponse()
         data = res.read()
         print(data)
