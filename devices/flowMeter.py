2a637e29ec3ccb521c010fd6d0495f65951935f4
Nicolás Dazeo <nicodazeo@gmail.com>
2018-04-07 11:40:21 -0300

    Added print from flowmeter

--- devices/flowMeter.py
+++ devices/flowMeter.py
@@ -4,6 +4,7 @@
 from threading import Thread
 from time import sleep
 from .observable import Observable
+from threading import Lock
 
 class FlowMeter(Thread, Observable):
     """
@@ -13,17 +14,49 @@ class FlowMeter(Thread, Observable):
         super().__init__()
         self.serial = serial
         self.daemon = True
-        self.eventListeners = []
+        self.serialLock = Lock()
         self.start()
 
     def reset(self):
+        self.serialLock.acquire()
         self.serial.write(b'FM+RST.ACM\n')
+        self.serialLock.release()
+
+    def print(self):
+        '''
+        Prints lines
+
+        :return: Ticket number
+        :rtype: Int
+        '''
+        self.serialLock.acquire()
+        self.serial.write(b'FM+PTR\n')
+        data = self.serial.readline()
+        self.serialLock.release()
+        return int(data.strip())
+
+    def setLabel(self, position, value):
+        '''
+        Sets printer label
+
+        :param int position: The position of the label in the ticket.
+        :param str value: The text to be printed. Must be less than 24 chars.
+        :return: Ticket label changed
+        :rtype: str
+        '''
+        self.serialLock.acquire()
+        self.serial.write('FM+LAB{0}={1}\n'.format(position, value).encode())
+        data = self.serial.readline()
+        self.serialLock.release()
+        return data
 
     def run(self):
         v0 = 0.0
         while True:
+            self.serialLock.acquire()
             self.serial.write(b'FM+ACM\n')
             data = self.serial.readline()
+            self.serialLock.release()
             if len(data)>0:
                 try:
                     v = float(data.strip()) # Needs timeout in serial for security
